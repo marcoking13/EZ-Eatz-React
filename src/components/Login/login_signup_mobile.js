5b1c96e027e7c042a1bb18a526b9a2f7525ed3ae
marcoking13 <marcokhodr16@gmail.com>
2019-12-10 10:12:44 -0800

    fixed some layout issues

--- src/components/Login/login_signup_mobile.js
+++ src/components/Login/login_signup_mobile.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Logo from "./../../images/logo.png";
-import Footer "./../Footer/footnote.js";
+import Footer from "./../Footer/footnote.js";
 
 class LoginSignupMobile extends React.Component{
   //JSX for the login and signup page mobile
+
+  renderErrMessage(err){
+    if(err){
+      return <p className="text-center cr">Wrong Username or Password </p>
+    }else{
+      return null
+    }
+  }
   render(){
     return(
       <div className="container-fluid bGeo pb400px">
@@ -21,6 +29,7 @@ class LoginSignupMobile extends React.Component{
             <p className="cb text-center">To your EZ account</p>
 
           <div className="inputBox ">
+              {this.renderErrMessage(this.props.err)}
               <input className="redLineInput w100 mt5 mb5 text-center" style={{borderBottomColor:this.props.className}} onChange = {(event)=>{this.props.changeUsername(event)}} value = {this.props.username}placeholder = "Type Username" />
               <input className="redLineInput w100 mt5 mb5 text-center" style={{borderBottomColor:this.props.className2}} onChange = {(event)=>{this.props.changePassword(event)}} value = {this.props.password} type="password" placeholder = " Type  Password" />
               <p className="text-center turnBlue f12px" onClick = {(e)=>{this.props.changeURL("usersign")}}>Create account here</p>
