0e85d1a1e0a25aee4760b62137509775c90bf8f0
marcoking13 <marcokhodr16@gmail.com>
2019-12-21 18:43:18 -0800

    l

--- src/components/Maps/mobile_bar.js
+++ src/components/Maps/mobile_bar.js
@@ -14,24 +14,23 @@ class MobileBar extends React.Component {
           <NavbarMobile changeURL = {this.props.changeURL} />
 
               <div className="row bb">
-                <div className="col-1"/>
-                <div className="col-1 p0">
-                </div>
-                <div className="col-1"/>
-                <div className="col-4">
+
+                <div className="col-3">
+
+                <div className="col-6">
                   <br />
                   <input  value = {this.props.place} className="form-control bb text-center cw "
                     onChange = {(e)=>{
 
                       var value = e.target.value;
-                      this.props.changePlace(value);
-                      this.props.ConvertToCoords(this.props.place);
+
+                      this.props.ConvertToCoords(value);
                     }}
                     placeholder = " Enter Address"/>
-                  </div>
-                  <div className="col-3 p0">
+
+                  <div className="p0">
                     <br />
-                      <button className="ui button inverted   w100 blue" onClick = {
+                      <button className="ui button inverted w0 blue" onClick = {
                         (e)=>{
 
                           this.props.ConvertToCoords(this.props.place);
@@ -39,7 +38,8 @@ class MobileBar extends React.Component {
                         }>Search
                       </button>
                     </div>
-                    <div className="col-1"/>
+                  </div>
+                    <div className="col-3"/>
                 </div>
 
           </div>
