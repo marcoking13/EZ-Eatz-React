292714f7923675b8f9724e768462a5069e248cf5
marcoking13 <marcokhodr16@gmail.com>
2023-02-25 20:10:52 -0700

    l

--- src/components/Admin/Landing/new_landing_mobile.js
+++ src/components/Admin/Landing/new_landing_mobile.js
@@ -36,19 +36,15 @@ class AdminLandingMobile extends React.Component{
     return(
       <div className="row" >
 
-        <div className="col-5"></div>
-
-
-        <div className="col-2"/>
-
-        <div className="col-4 margin-top-2_5">
+        <div className="col-3"/>
+        <div className="col-9 margin-top-2_5">
           <div className="row">
-            <div className="col-6">
-              <button className="width-100  btn btn_landing " onClick={()=>{this.props.ChangeURL("/admin/login")}}>Login</button>
+            <div className="col-5">
+              <button className="width-100  btn btn_landing " onClick={()=>{this.props.ChangeURL("/admin/login")}} style={{fontSize:"14px"}}>Login</button>
             </div>
 
             <div className="col-5">
-              <button onClick={()=>{this.props.ChangeURL("/admin/signup")}} className="width-100  btn signup_button_landing btn_landing black-background white">Create Account</button>
+              <button onClick={()=>{this.props.ChangeURL("/admin/signup")}} className="width-100  btn signup_button_landing btn_landing black-background white"style={{fontSize:"14px"}}>Signup</button>
             </div>
 
           </div>
@@ -64,11 +60,10 @@ class AdminLandingMobile extends React.Component{
   renderBubble1(){
     return(
       <div className="row">
-        <div className="col-4">
+        <div className="col-6">
           <img className="width-100" src = {Logo1}/>
         </div>
-        <div className="col-1"/>
-        <div className="col-5">
+        <div className="col-6">
           <p className="admin_show_title">Become an Owner</p>
           <p className="admin_show_description">
           As a delivery driver, you'll make reliable money—working anytime, anywhere.
@@ -84,11 +79,10 @@ class AdminLandingMobile extends React.Component{
   renderBubble3(){
     return(
       <div className="row">
-        <div className="col-4">
+        <div className="col-6">
           <img className="width-100" src = {Logo3}/>
         </div>
-        <div className="col-1"/>
-        <div className="col-5">
+        <div className="col-6">
           <p className="admin_show_title">Showcase Your Menu</p>
           <p className="admin_show_description">
           Experience the best your neighborhood has to offer, all in one app.
@@ -107,11 +101,11 @@ class AdminLandingMobile extends React.Component{
     renderBubble2(){
       return(
         <div className="row">
-          <div className="col-4">
+          <div className="col-6">
             <img className="width-100" src = {Logo2}/>
           </div>
-          <div className="col-1"/>
-          <div className="col-5">
+
+          <div className="col-6">
             <p className="admin_show_title">Become a Partner</p>
             <p className="admin_show_description">
             Grow your business and reach new customers by partnering with us.
@@ -131,14 +125,14 @@ class AdminLandingMobile extends React.Component{
         <div className="col-1"/>
         <div className="col-10">
           <div className="row">
-            <div className="col-4">
+            <div className="col-6">
                 <p className="admin_info_title">All in one app.</p>
                 <p className="admin_info_description">
                 Discover local, on-demand delivery or Pickup from restaurants, nearby grocery and convenience stores, and more.
                 </p>
 
             </div>
-            <div className="col-8">
+            <div className="col-6">
                 <img className=" show_image_admin" style={{position:"relative",top:"50px"}} src = {Show1} />
             </div>
 
@@ -155,10 +149,10 @@ class AdminLandingMobile extends React.Component{
         <div className="col-10">
           <div className="row">
 
-            <div className="col-8">
+            <div className="col-6">
                 <img className=" show_image_admin" src = {Show2} />
             </div>
-            <div className="col-4">
+            <div className="col-6">
                 <p className="admin_info_title text_right">Every Flavor Welcome</p>
                 <p className="admin_info_description text_right">
                 From your neighborhood sushi spot to the burger and fries you crave, choose from over 300,000 local and national favorites across the U.S., Canada and Australia.
@@ -179,9 +173,14 @@ class AdminLandingMobile extends React.Component{
 
       <div className="background_landing_container width-100"style={{height:"700px",background:`url(${LandingBackground})`}}>
         {this.renderNavbar()}
-        <div className="width-100 margin-top-10">
-          <p className="ez_title text-center medium-font margin-top-2_5 cw bottom-30px">Admin</p>
-          <img className="width-100" src={Logo} />
+        <div className="row margin-top-5">
+          <div className="col-12 margin-top-10">
+            <p className="ez_title text-center medium-font margin-top-2_5 cw bottom-30px">EZ-Eatz</p>
+          </div>
+          <div className="col-3"/>
+          <div className="col-6">
+            <img className="width-100" src={Logo} />
+          </div>
         </div>
 
         <div className="row">
@@ -214,16 +213,16 @@ class AdminLandingMobile extends React.Component{
         </div>
 
         <div className="row">
-          <div className="col-2"/>
-          <div className="col-8">
+          <div className="col-1"/>
+          <div className="col-10">
             <div className="row">
-              <div className="col-12">
+              <div className="col-12 margin-top-10">
                 {this.renderBubble1()}
               </div>
-              <div className="col-12">
+              <div className="col-12 margin-top-10">
                 {this.renderBubble2()}
               </div>
-              <div className="col-12">
+              <div className="col-12 margin-top-10">
                 {this.renderBubble3()}
               </div>
             </div>
